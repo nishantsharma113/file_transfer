73ac09ce9ab7e14b78851bd6103e66839fcb0918
nishantsharma113 <nishantsharma11398@gmail.com>
2025-04-02 11:16:28 +0530

    Updates

---
+++ static/js/198.06fda269.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfile_transfer_app=self.webpackChunkfile_transfer_app||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.06fda269.chunk.js.map
