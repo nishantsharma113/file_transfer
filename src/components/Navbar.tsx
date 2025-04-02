99b77c9081458411dcf738df73b22fabe9248dc1
nishantsharma113 <nishantsharma11398@gmail.com>
2025-04-02 12:39:20 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,12 +42,12 @@ const Navbar = ({ user }: NavbarProps) => {
               >
                 Upload
               </Link>
-              <Link
+              {/* <Link
                 to="/files"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
                 My Files
-              </Link>
+              </Link> */}
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
